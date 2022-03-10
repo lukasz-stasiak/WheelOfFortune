275c8ba8a2ad0b55358b510ab924f46ba57cec10
Lukasz <uzytkownik@kursGIT.pl>
2022-03-10 21:23:07 +0100

    US05 - Dodanie graczy

--- src/main/java/pl/javagame/App.java
+++ src/main/java/pl/javagame/App.java
@@ -1,9 +1,47 @@
 package pl.javagame;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Witaj w Kole Fortuny");
+
+        ArrayList<Player> listaGraczy = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int iloscGraczy = sprawdzIlloscGraczy();
+
+        try {
+            for (int i = 1; i < iloscGraczy + 1; i++) {
+                System.out.println("Wpisz imię gracza nr: " + i);
+                Player imie = new Player(scanner.nextLine());
+                listaGraczy.add(imie);
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Nie podałeś imienia gracza");
+        }
+
+        System.out.println("Lista graczy: ");
+        for (Player lista : listaGraczy) {
+            System.out.println("Gracz: " + lista);
+        }
+        scanner.close();
+
+    }
+
+    private static int sprawdzIlloscGraczy() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj ilość graczy: ");
+        int iloscGraczy = scanner.nextInt();
+
+        while (iloscGraczy < 2 || iloscGraczy > 4) {
+            System.out.println("Ilość graczy ma być między 2 i 4");
+            System.out.print("Podaj ilość graczy: ");
+            iloscGraczy = scanner.nextInt();
+        }
+        return iloscGraczy;
+
 
     }
 }
