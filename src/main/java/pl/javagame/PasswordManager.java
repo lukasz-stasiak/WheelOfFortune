57864a03545f317a1271c5b8ea4bdb0bcbe2d97f
Lukasz <uzytkownik@kursGIT.pl>
2022-04-09 10:52:31 +0200

    US08 Losowanie unikalnych haseł, HashMap?

--- src/main/java/pl/javagame/PasswordManager.java
+++ src/main/java/pl/javagame/PasswordManager.java
@@ -1,35 +1,70 @@
 package pl.javagame;
 
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class PasswordManager {
 
 
-    private ArrayList<String> passwords = new ArrayList<>();
+    private HashMap<String, Boolean> passwords = new HashMap<>();
+    // private ArrayList<String> copyPasswords = new ArrayList<>();
 
     public PasswordManager() {
-        passwords.add("Apetyt rośnie w miarę jedzenia");
-        passwords.add("Co dwie głowy to nie jedna");
-        passwords.add("Cwiczenie czyni mistrza");
-        passwords.add("Darowanemu koniowi w zęby się nie zagląda");
-        passwords.add("Diabeł tkwi w szczegółach");
-        passwords.add("Elektryka prąd nie tyka");
+        passwords.put("Apetyt rośnie w miarę jedzenia", false);
+        passwords.put("Co dwie głowy to nie jedna", false);
+        passwords.put("Cwiczenie czyni mistrza", true);
+        passwords.put("Darowanemu koniowi w zęby się nie zagląda", false);
+        //    passwords.add("Diabeł tkwi w szczegółach");
+        //    passwords.add("Elektryka prąd nie tyka");
 
-        getRandomPassword();
+        //    getRandomPassword();
     }
 
 
     public String getRandomPassword() {
-        int max = passwords.size(); // result=6
-        int i = (int) (Math.random() * max);
-        String randomPassword = passwords.get(i);
-        passwords.remove(i); // to avoid the same passwords
-        return randomPassword;
 
+        String Password = null;
+        String getKey;
+        Boolean getValue;
+
+        Integer max = passwords.keySet().toArray().length;// result=6
+        //  int random = new Random().nextInt(max);
+
+        //  System.out.println(Password);
+
+        Object randomPassword = passwords.keySet().toArray()[new Random().nextInt(max)];
+
+      //  randomPassword = checkigRandomPassword(randomPassword);
+
+        for (Map.Entry<String, Boolean> map : passwords.entrySet()) {
+
+            getKey = map.getKey();
+            if (getKey.equals(randomPassword) && map.getValue() == true) {
+                randomPassword = passwords.keySet().toArray()[new Random().nextInt(max)];
+            } else if (getKey.equals(randomPassword) && map.getValue() == false) {
+                Password = getKey;
+                map.setValue(true);
+            }
+        }
+
+        return Password;
     }
 
+    public Object checkigRandomPassword(Object randomPassword2) {
+        for (Map.Entry<String, Boolean> map : passwords.entrySet()) {
+            Integer max = passwords.keySet().toArray().length;
+            String getKey;
+            getKey = map.getKey();
+            if (getKey.equals(randomPassword2) && map.getValue() == true) {
+                randomPassword2 = passwords.keySet().toArray()[new Random().nextInt(max)];
+                checkigRandomPassword(randomPassword2);
+            }
+        }
+        return randomPassword2;
+    }
+
+
 }
+
 
 
